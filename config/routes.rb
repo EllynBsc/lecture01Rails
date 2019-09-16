36af36b481917732175186fbaba20a85f9763435
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2019-09-16 09:40:17 +0200

    pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # get 'pages/about'
+
+  get '/about', to: 'pages#about'
+  # verb '/path', to: 'controller_name#action_name'
+
+  # get 'pages/contact'
+
+  get '/contact', to: 'pages#contact'
+  # get '/home', to: 'pages#home'
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
